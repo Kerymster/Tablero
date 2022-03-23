5d0c577864d2e15455efc469ddb4e19560c59d3e
Kerymster <kerimyilmaz5270@gmail.com>
2022-03-23 10:00:03 +0300

    basic html structure is created

--- src/components/navbar/NavigationBar.jsx
+++ src/components/navbar/NavigationBar.jsx
@@ -1,7 +1,44 @@
 import React from "react";
 import "./NavigationBar.css";
+import SearchIcon from "@mui/icons-material/Search";
+import LanguageRoundedIcon from "@mui/icons-material/LanguageRounded";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 const NavigationBar = () => {
-  return <div>NavigationBar</div>;
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Busca.." />
+          <SearchIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageRoundedIcon />
+            English
+          </div>
+          <div className="item">
+            <LanguageRoundedIcon />
+            English
+          </div>
+          <div className="item">
+            <LanguageRoundedIcon />
+            English
+          </div>
+          <div className="item">
+            <LanguageRoundedIcon />
+            English
+          </div>
+          <div className="item">
+            <LanguageRoundedIcon />
+            English
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavigationBar;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Sidebar from "../../components/aside/Sidebar";
+import NavigationBar from "../../components/navbar/NavigationBar";
 import "./Home.css";
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
-      <div className="homeContainer">Content</div>
+      <div className="homeContainer">
+        <NavigationBar />
+        homeContainer
+      </div>
     </div>
   );
 };
