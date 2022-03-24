b896eb280008070f791c7a046a433fbe7238088f
Kerymster <kerimyilmaz5270@gmail.com>
2022-03-24 12:26:38 +0300

    Adding panel infrastructure is being created

--- src/pages/addnew/Addnew.jsx
+++ src/pages/addnew/Addnew.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Sidebar from "../../components/aside/Sidebar";
+import NavigationBar from "../../components/navbar/NavigationBar";
 import "./Addnew.css";
 const Addnew = () => {
-  return <div>Addnew</div>;
+  return (
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <NavigationBar />
+        test
+      </div>
+    </div>
+  );
 };
 
 export default Addnew;
